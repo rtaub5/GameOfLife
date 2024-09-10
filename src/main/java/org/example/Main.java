a75d8030f901ed669a66733717360a7d19e8b5e7
ROCKI TAUB <racheltaub28@gmail.com>
2024-09-09 21:18:31 -0400

    added gameoflife.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,18 @@ public class Main
     {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++)
-        {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+            int [][] gameBoard = {
+                    {0, 0, 0},
+                    {1, 1, 1},
+                    {0, 0, 0}
+            };
+
+            GameOfLife game = new GameOfLife(gameBoard);
+            game.nextGeneration();
+
+
+
         }
     }
-}
