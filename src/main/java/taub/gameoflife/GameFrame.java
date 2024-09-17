90a23103149c961f0d977885b3014e220ec48483
ROCKI TAUB <racheltaub28@gmail.com>
2024-09-16 20:29:03 -0400

    fixed styles

--- src/main/java/taub/gameoflife/GameFrame.java
+++ src/main/java/taub/gameoflife/GameFrame.java
@@ -10,19 +10,18 @@ public class GameFrame extends JFrame
 {
     public GameFrame()
     {
-
         setSize(80, 400);
         setTitle("Board");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        int rows = 25;
-        int cols = 53;
-
         JPanel buttonPanel = new JPanel();
         JButton playButton = new JButton("Play");
         buttonPanel.add(playButton);
         playButton.setPreferredSize(new Dimension(100, 100));
 
         setLayout(new BorderLayout());
+
+        int rows = 25;
+        int cols = 53;
         GameOfLifeGrid grid = new GameOfLifeGrid(rows, cols);
         add(grid);
         add(buttonPanel, BorderLayout.SOUTH);
