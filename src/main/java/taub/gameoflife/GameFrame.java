d9d75e85debfdabc838ae6b873b257c7db455f16
ROCKI TAUB <racheltaub28@gmail.com>
2024-09-18 14:17:21 -0400

    added timer

--- src/main/java/taub/gameoflife/GameFrame.java
+++ src/main/java/taub/gameoflife/GameFrame.java
@@ -8,8 +8,18 @@ import java.awt.event.ActionListener;
 
 public class GameFrame extends JFrame
 {
-
-
+    int rows = 25;
+    int cols = 53;
+    GameOfLifeGrid grid = new GameOfLifeGrid(rows, cols);
+    Timer timer = new Timer(1000, new ActionListener()
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            grid.game.nextGeneration();
+            grid.repaint();
+        }
+    });
     public GameFrame()
     {
         setSize(80, 400);
@@ -17,14 +27,14 @@ public class GameFrame extends JFrame
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel buttonPanel = new JPanel();
         JButton playButton = new JButton("Play");
+        JButton stopButton = new JButton("Stop");
         buttonPanel.add(playButton);
+        buttonPanel.add(stopButton);
         playButton.setPreferredSize(new Dimension(100, 100));
+        stopButton.setPreferredSize(new Dimension(100, 100));
 
         setLayout(new BorderLayout());
 
-        int rows = 25;
-        int cols = 53;
-        GameOfLifeGrid grid = new GameOfLifeGrid(rows, cols);
         add(grid);
         add(buttonPanel, BorderLayout.SOUTH);
 
@@ -39,8 +49,16 @@ public class GameFrame extends JFrame
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                grid.game.nextGeneration();
-                grid.repaint();
+                timer.start();
+            }
+        });
+
+        stopButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                timer.stop();
             }
         });
 
